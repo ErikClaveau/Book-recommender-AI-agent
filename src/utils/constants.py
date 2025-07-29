1d6948fabb3c8b65da9a5681d9c46b3d19a68b1f
ErikClaveau <71637176+ErikClaveau@users.noreply.github.com>
2025-07-29 18:59:18 +0200

    Added parallelization to the nodes (#7)

--- src/utils/constants.py
+++ src/utils/constants.py
@@ -5,10 +5,11 @@ THINKING_NODE: str = "thinking_node"
 SAVE_RECOMMENDED_BOOKS: str = "save_recommended_books_node"
 SAVE_PREFERENCES: str = "save_preferences_node"
 SAVE_READ_BOOKS: str = "read_books_node"
+EMPTY_NODE: str = "empty_node"
 
 # Initial router tags
 INITIAL_ROUTER_TAGS: dict = {
-    "recommendation": SAVE_RECOMMENDED_BOOKS,
+    "recommendation": EMPTY_NODE,
     "preferences": SAVE_PREFERENCES,
     "talk": THINKING_NODE,
     "read": SAVE_READ_BOOKS,
